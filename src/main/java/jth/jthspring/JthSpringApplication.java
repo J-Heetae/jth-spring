3504926db24418cde5f8e0537f4c3767e1a13335
goose <th1563@naver.com>
2022-06-03 23:54:01 +0900

    Initial commit

---
+++ src/main/java/jth/jthspring/JthSpringApplication.java
@@ -0,0 +1,13 @@
+package jth.jthspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JthSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JthSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jth/jthspring/JthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jth.jthspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
